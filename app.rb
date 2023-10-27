2be215e501199f90c332eea565abbd5813d1c357
dinerograham <102576939+dinerograham@users.noreply.github.com>
2023-10-27 04:12:47 +0000

    payment calculator doesn't work

--- app.rb
+++ app.rb
@@ -2,8 +2,57 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+  erb(:sqrd_form)
+end
+
+get("/square/new") do
+  erb(:sqrd_form)
+end
+
+get("/square/results") do
+  @root = params.fetch("number").to_f
+  @perfect = @root**2.0
+  erb(:squarer)
+end  
+
+get("/square_root/new") do
+  erb(:sqrt_form)
+end  
+
+get("/square_root/results") do
+  @square = params.fetch("number").to_f
+  @sq_root = @square**0.5
+  erb(:sqrt)
+end  
+
+get("/payment/new") do
+  erb(:finance_form)
+end
+
+get("/payment/results") do
+  @principal = params.fetch("user_pv").to_i
+  .to_formatted_s(:currency)
+  @years = params.fetch("user_years").to_i
+  @apr = params.fetch("user_apr").to_f.to_formatted_s(:percentage, precision: 4)
+  @rate = @apr./100
+  @numer = @principal*@rate
+  @periods = @years*12
+  @expo = -@periods
+  @oneplus = 1+@rate
+  @minus = @oneplus**@periods
+  @denom = 1-@minus
+  @payment = @numer/@denom
+  erb(:finance)
+end  
+
+get("/random/new") do
+  erb(:random_form)
+end
+
+get("/random/results") do
+  @min = params.fetch("user_min").to_f
+  @max = params.fetch("user_max").to_f
+  @ran_num = rand(@min..@max)
+  erb(:random)
 end
